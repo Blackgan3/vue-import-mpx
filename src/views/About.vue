2950b9891a8d3bffcd4e1c5f2695a464e7bd1ba7
xuegan <xuegan@didiglobal.com>
2023-11-22 17:08:02 +0800

    feat: upgrade Vue

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <span>{{ count }}</span>
+  </div>
+</template>
+<script lang="js">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  created() {
+    this.count++
+    console.log('count:', this.count)
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    hello world
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+  },
+});
+</script>
