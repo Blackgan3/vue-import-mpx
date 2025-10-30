a08ff0005d1d943b50d8f123c004d344be5f2e99
xuegan <xuegan@didiglobal.com>
2025-10-30 17:04:52 +0800

    feat: 增加i18n使用示例

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <Button @tap="testButtonTap">hello world</Button>
+    <Hello />
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
 import Button from '@didi/turbo-ui/lib/button/button.mpx?isComponent';
+import Hello from '../components/helloWorld.mpx?isComponent'
 export default Vue.extend({
   name: 'Home',
   components: {
-    Button
+    Button,
+    Hello
   },
   methods: {
     testButtonTap() {
